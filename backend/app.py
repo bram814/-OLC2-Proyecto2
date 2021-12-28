e61a47fe05106b164f5aadb853cc1178703a330b
Abraham Solórzano <abraham81423@gmail.com>
2021-12-27 18:29:20 -0600

    [ADD] Backend

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+route = '/api'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route(f'{route}/p', methods=['GET'])
+def home():
+    return "HOME"
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000, debug=True)
